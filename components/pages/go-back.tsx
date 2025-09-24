4ef99d2d2d5ce1dcff89130c7a12c6da24206fb6
Md. Shohag Hossain <shohag@atilimited.net>
2025-09-24 14:37:22 +0600

    Update go-back.tsx

--- components/pages/go-back.tsx
+++ components/pages/go-back.tsx
@@ -1,16 +1,20 @@
 "use client";
-import Link from "next/link";
+
 import { useRouter } from "next/navigation";
 
-export default function GoBack() {
+interface Props{
+  name?:string
+}
+
+export default function GoBack({name}:Props) {
   const route = useRouter()
   return (
     <>
       <button
         onClick={()=>{route.back()}}
-        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        className="rounded-md bg-[#0c2355] px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-[#031c53] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
-        Go back
+        {name || 'Go Back'}
       </button>
     </>
   );
