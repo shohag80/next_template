a51dfc5f3bb5e0d038af83dcd6d004d4abeb553a
Md. Shohag Hossain <shohag@atilimited.net>
2025-09-24 18:32:37 +0600

    new user

--- app/(backend)/users/new-user/page.tsx
+++ app/(backend)/users/new-user/page.tsx
@@ -58,7 +58,7 @@ export default function NewUserPage() {
   }
 
   return (
-    <div>
+    <>
       <ToastContainer />
       <header className="relative shadow-sm">
         <div className="flex justify-between mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
@@ -181,6 +181,6 @@ export default function NewUserPage() {
           </div>
         </div>
       </main>
-    </div>
+    </>
   );
 }
